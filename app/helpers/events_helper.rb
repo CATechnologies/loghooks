1945c49e84ddbf68b5bf594fafe0b9287febb0f1
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-16 18:46:00 +0200

    nice output for GitHub payload

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,7 +6,17 @@ module EventsHelper
     if event.data.blank?
       event.data.inspect
     else
-      YAML.dump(event.data).sub(/.+\n/, '')
+      if event.data.keys == %w[payload]
+        data = ActiveSupport::JSON.decode event.data['payload']
+        comment = 'JSON-encoded string received under "payload" key'
+      else
+        data = event.data
+        comment = nil
+      end
+      
+      output = YAML.dump(data).sub(/.+\n/, '')
+      output = "# #{comment}\n" + output if comment
+      output
     end
   end
   
