d55a7c0404ec032a452aee412bdf3acf4bdb50c5
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-16 15:21:11 +0200

    fix Sass stylesheets on Heroku

--- config/initializers/haml_sass.rb
+++ config/initializers/haml_sass.rb
@@ -1,12 +1,17 @@
 heroku = !!ENV['HEROKU_TYPE']
-
-Sass::Plugin.options[:template_location] = {
-  "#{Rails.root}/app/styles" => heroku ?
-    "#{Rails.root}/tmp/stylesheets" : "#{Rails.root}/public/stylesheets"
-}
+controller_config = ActionController::Base.config
+rails_config = Rails.configuration
 
 if heroku
+  controller_config.stylesheets_dir = File.join(ENV['TMPDIR'], 'stylesheets')
+  rails_config.action_dispatch.x_sendfile_header = ''
+  rails_config.serve_static_assets = true
+  
   # add Rack middleware to serve compiled stylesheets from "tmp/stylesheets"
-  Rails.configuration.middleware.insert_after 'Sass::Plugin::Rack', 'Rack::Static',
-    :urls => ['/stylesheets'], :root => "#{Rails.root}/tmp"
+  rails_config.middleware.insert_after 'Sass::Plugin::Rack', 'Rack::Static',
+    :urls => ['/stylesheets'], :root => ENV['TMPDIR']
 end
+
+Sass::Plugin.options[:template_location] = {
+  "#{Rails.root}/app/styles" => controller_config.stylesheets_dir
+}
